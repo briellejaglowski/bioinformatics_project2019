5a005a6b34299cbe1abc26c5c2d52f74a8500bbb
Brielle Jaglowski <bjaglows@crcfe01.crc.nd.edu>
2019-10-16 11:05:20 -0400

    muscle code added

--- bioinformProj.sh
+++ bioinformProj.sh
@@ -4,7 +4,7 @@
 #cat all reference sequences into one file
 cat "$@" >> "$combined.fasta"
 #Use muscle to align reference sequences.
-
+./muscle3.8.31_i86linux.32 -in $combined.fasta -out $aligned.fasta
 #hmmbuild
 
 #hmmsearch

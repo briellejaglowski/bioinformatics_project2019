da2571e2b47fc4ea30af64daf0562939569db97c
John Caffarelli <jcaffare@crcfe01.crc.nd.edu>
2019-10-18 12:46:00 -0400

    changed input/output file names

--- bioinformProj.sh
+++ bioinformProj.sh
@@ -4,18 +4,19 @@
 
 #mcrA
 #cat all mcrA gene reference sequences into one file
-#usage: "$@" = ref_sequences/mcrAgene_$$.fasta
-cat "$@" > combined.fasta
+#usage: "$@" = ref_sequences/mcrAgene_*.fasta
+#This works!
+cat "$@" > ref_sequences/combmcrA.fasta
 
 #muscle to align reference sequences
-./muscle3.8.31_i86linux64 -in combined.fasta -out aligned.afa
+./muscle3.8.31_i86linux64 -in ref_sequences/combmcrA.fasta -out aligned.afa
 
 # Path to hmmr files
 PATH=$PATH:~/Private/bin/bin
  
 #hmmbuild
-hmmbuild mcrabuilt.hmm aligned.fasta
+hmmbuild mcrabuilt.hmm aligned.afa
 
 #hmmsearch
-#hmmsearch -g mcrabuilt.hmm aligned.fasta > mcra.out
+#hmmsearch -g mcrabuilt.hmm aligned.afa > mcra.out
 

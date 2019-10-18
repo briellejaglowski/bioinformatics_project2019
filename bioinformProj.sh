4cc1b20d965cb91ed33b36094fd127042250f0de
Brielle Jaglowski <bjaglows@crcfe01.crc.nd.edu>
2019-10-17 21:53:08 -0400

    hmmbuild and hmmsearch code

--- bioinformProj.sh
+++ bioinformProj.sh
@@ -2,9 +2,11 @@
 
 #mcrA
 #cat all reference sequences into one file
-cat "$@" >> "$combined.fasta"
+cat *.fasta >> "combined.fasta"
 #Use muscle to align reference sequences.
-./muscle3.8.31_i86linux.32 -in $combined.fasta -out $aligned.fasta
+./muscle3.8.31_i86linux.32 -in combined.fasta -out aligned.fasta
 #hmmbuild
-
+hmmbuild mcrabuilt.hmm aligned.fasta
 #hmmsearch
+hmmsearch -g mcrabuilt.hmm aligned.fasta > mcra.out
+

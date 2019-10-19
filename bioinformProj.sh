67e527b203cd975f0368bab89e9c45a6e0968caa
John Caffarelli <jcaffare@crcfe01.crc.nd.edu>
2019-10-18 20:23:59 -0400

    finished the project

--- bioinformProj.sh
+++ bioinformProj.sh
@@ -1,11 +1,13 @@
 
 #shell script for Bioinformatic projects
-#usage: bash bioinformProj.sh "$1"
+#usage: bash bioinformProj.sh "$1" "$2"
+# initiate final file
+echo: "Number,mcrA,hsp70" > finaloutput.txt
 
 #mcrA
+
 #cat all mcrA gene reference sequences into one file
 #usage: "$1" = ref_sequences/mcrAgene_*.fasta
-#This works!
 cat "$1" > ref_sequences/combmcrA.fasta
 
 #muscle to align mcrAgene reference sequences
@@ -17,33 +19,34 @@ PATH=$PATH:~/Private/bin/bin
 #hmmbuild for aligned mcrAgene
 hmmbuild mcrabuilt.hmm alignmcrA.afa
 
-#hmmsearch for mcrAgene in proteomes
-
 #for loop with hmmsearch to compare with built aligned mcrA sequences with proteomes 1 through 50
-for N in {01..50}
+#set variable mcrA to output of hmmbuild with matches
+for Number in {01..50}
 do
-hmmsearch mcrabuilt.hmm proteomes/proteome_$N.fasta >> mcra.out
+hmmsearch --tblout mcrA_$Number.out mcrabuilt.hmm proteomes/proteome_$Number.fasta  
+mcrA=$(cat mcrA_$Number.out | grep -v '#' -m 1 | wc -l)
 done
 
-#need to insert code here to select suitable proteomes from mcra outputs
-cat mcra.out | grep -v '^#' > selected.out 
-
 #hsp70
+
 #cat all hsp70 gene reference sequences into one file
 #usage: "$2" = ref_sequences/hsp70gene_*.fasta
-#cat "$2" > ref_sequences/combhsp70.fasta
+cat "$2" > ref_sequences/combhsp70.fasta
 
 #muscle to align hsp70gene reference sequence
-#./muscle3.8.31_i86linux64 -in ref_sequences/combhsp70.fasta -out alignhsp70.afa
+./muscle3.8.31_i86linux64 -in ref_sequences/combhsp70.fasta -out alignhsp70.afa
 
 #Path to hmmr files
-#PATH=$PATH:~/Private/bin/bin
+PATH=$PATH:~/Private/bin/bin
 
 #hmmbuild for aligned hsp70gene
-#hmmbuild hsp70built.hmm alignhsp70.afa
+hmmbuild hsp70built.hmm alignhsp70.afa
 
-#for loop with hmmsearch to compare with build aligned hsp70gene sequences with proteomes 1 through 50
-#for N in {01..50}
-#do
-#hmmsearch hsp70built.hmm proteomes/proteome_$N.fasta > hsp70.out
-#done 
+#for loop with hmmsearch to identify matches
+#set variable hsp70 to output of hmmbuild with matches
+for Number in {01..50}
+do
+hmmsearch --tblout hsp70_$Number.out hsp70built.hmm proteomes/proteome_$Number.fasta 
+hsp70=$(cat hsp70_$Number.out | grep -v '#' | wc -l )
+echo "$Number,$mcrA,$hsp70" >> finaloutput.txt
+done 
